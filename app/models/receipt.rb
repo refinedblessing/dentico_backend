b1ac18a3cfc13463b56a7caab412913a6f500281
Blessing Ebowe <blessingebowe@gmail.com>
2018-01-23 14:49:25 +0100

    Update receipt model to only create when an appointment has been completed.

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -3,4 +3,12 @@ class Receipt < ApplicationRecord
   validates_associated :appointment
   enum payment_type: [:cash, :debit, :credit], _suffix: true
   default_scope -> { order(created_at: :desc) }
+  validate :appointment_is_completed
+
+  private
+
+  def appointment_is_completed
+    appointment = Appointment.find(self.appointment_id)
+    errors.add(:appointment, "needs to be booked.") if appointment.status != "completed"
+  end
 end
